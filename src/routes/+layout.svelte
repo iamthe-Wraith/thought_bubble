144efd8abb044881921c3ff4cefef380cad20ff8
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-12-08 23:22:02 -0500

    adds init colors and page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import './style.css';
+  import dayjs from 'dayjs';
+
+  const now = dayjs();
+</script>
+
+<header>
+  <a href="/">Thought Bubble</a>
+</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <div>© {now.format('YYYY')}</div>
+</footer>
+
+<style>
+  header,
+  footer {
+    display: flex;
+    align-items: center;
+    height: 5rem;
+    padding: 1rem;
+  }
+
+  header {
+    background: var(--primary);
+  }
+
+  main {
+    min-height: calc(100vh - 10rem);
+    padding: 1rem;
+  }
+
+  footer {
+    border-top: 1px solid var(--primary);
+  }
+</style>
